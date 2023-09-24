b74a087ffd6fa288403a55fb6c07fe9c02504785
dfrincong <139162742+dfrincong@users.noreply.github.com>
2023-09-24 17:51:01 -0500

    feat: :construction: se realizan modificaciones
    modificaciones en index.html, main.js y se agrega style.css

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// style
-const borde = document.querySelector('table');
-borde.style.border = "solid";
-
 // funcionalidades
 const url = "https://650ee92e54d18aabfe999cc6.mockapi.io/table";
 
@@ -59,14 +55,18 @@ addEventListener("DOMContentLoaded", async(e)=>{
         // console.log(dato);
         table.insertAdjacentHTML("beforeend",`
         <tr>
-            <td>${dato.id}</td>
-            <td>${dato.money}</td>
+            <td class="end">${dato.id}</td>
+            <td class="end">${dato.money}</td>
             <td>${dato.opcion}</td>
             <td>
-                <button id="${dato.id}" class="editar">+-</button>
+                <button id="${dato.id}" class="editar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="m18.988 2.012 3 3L19.701 7.3l-3-3zM8 16h3l7.287-7.287-3-3L8 13z"></path><path d="M19 19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .896-2 2v14c0 1.104.897 2 2 2h14a2 2 0 0 0 2-2v-8.668l-2 2V19z"></path></svg>
+                </button>
             </td>
             <td>
-                <button id="${dato.id}" class="eliminar">x</button>
+                <button id="${dato.id}" class="eliminar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm4 12H8v-9h2v9zm6 0h-2v-9h2v9zm.618-15L15 2H9L7.382 4H3v2h18V4z"></path></svg>
+                </button>
             </td>
         </tr>
         `);
