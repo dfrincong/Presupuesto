9cc7137921d4dd9a14907b19219c75da4a9ee716
dfrincong <139162742+dfrincong@users.noreply.github.com>
2023-09-24 00:17:46 -0500

    feat: :sparkles: se agrega la información del json a la tabla

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 const borde = document.querySelector('table');
 borde.style.border = "solid";
 
-// funcionalidad
+// funcionalidades
 const url = "https://650ee92e54d18aabfe999cc6.mockapi.io/table";
+
+//enviar
 const formulario = document.querySelector("#formulario");
 formulario.addEventListener("submit",async(e)=>{
     e.preventDefault();
@@ -13,6 +15,27 @@ formulario.addEventListener("submit",async(e)=>{
         headers: {"content-type":"application/json"},
         body: JSON.stringify(fila)
     };
-    const resp = await (await fetch(url,config)).json();
+    let resp = await (await fetch(url,config)).json();
+    // console.log(resp);
+});
+
+// agregar a la tabla
+addEventListener("DOMContentLoaded", async(e)=>{
+    const table = document.querySelector("#datos");
+    let resp = await (await fetch(url)).json();
     console.log(resp);
+    resp.map(dato=>{
+        console.log(dato);
+        table.insertAdjacentHTML("beforeend",`
+        <tr>
+            <td>${dato.id}</td>
+            <td>${dato.money}</td>
+            <td>${dato.opcion}</td>
+            <td>
+                <button>+-</button>
+                <button>x</button>
+            </td>
+        </tr>
+        `)
+    })
 });
