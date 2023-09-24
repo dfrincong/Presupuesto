cc57653109bd606e2292ee08bac8f887707e2867
dfrincong <139162742+dfrincong@users.noreply.github.com>
2023-09-23 22:48:54 -0500

    feat: :sparkles: se guarda dato en mockapi

--- main.js
+++ main.js
@@ -2,3 +2,17 @@
 const borde = document.querySelector('table');
 borde.style.border = "solid";
 
+// funcionalidad
+const url = "https://650ee92e54d18aabfe999cc6.mockapi.io/table";
+const formulario = document.querySelector("#formulario");
+formulario.addEventListener("submit",async(e)=>{
+    e.preventDefault();
+    let fila = Object.fromEntries(new FormData(e.target));
+    let config = {
+        method: "POST",
+        headers: {"content-type":"application/json"},
+        body: JSON.stringify(fila)
+    };
+    const resp = await (await fetch(url,config)).json();
+    console.log(resp);
+});
