d3ae73d2181b5657684df822aa043264cdcb67f3
dfrincong <139162742+dfrincong@users.noreply.github.com>
2023-09-24 12:49:35 -0500

    feat: :sparkles: se introduce la función de eliminación

--- main.js
+++ main.js
@@ -15,27 +15,52 @@ formulario.addEventListener("submit",async(e)=>{
         headers: {"content-type":"application/json"},
         body: JSON.stringify(fila)
     };
-    let resp = await (await fetch(url,config)).json();
+    let resp = await (await fetch(url, config)).json();
     // console.log(resp);
+    location.reload();
 });
 
-// agregar a la tabla
+// eliminar
+const eliminarDato = async(id)=>{
+    let config = {
+      method: "DELETE",
+      headers: {"content-type":"application/json"}
+    };
+    let resp = await (await fetch(url + `/${id}`, config)).json();
+    // console.log(resp);
+    location.reload();
+  };
+
+// la tabla
 addEventListener("DOMContentLoaded", async(e)=>{
     const table = document.querySelector("#datos");
     let resp = await (await fetch(url)).json();
-    console.log(resp);
+    // console.log(resp);
     resp.map(dato=>{
-        console.log(dato);
+        // console.log(dato);
         table.insertAdjacentHTML("beforeend",`
         <tr>
             <td>${dato.id}</td>
             <td>${dato.money}</td>
             <td>${dato.opcion}</td>
             <td>
-                <button>+-</button>
-                <button>x</button>
+                <button id="${dato.id}" class="editar">+-</button>
+            </td>
+            <td>
+                <button id="${dato.id}" class="eliminar">x</button>
             </td>
         </tr>
-        `)
-    })
+        `);
+    });
+
+    const botonEliminar = document.querySelectorAll(".eliminar");
+    console.log(botonEliminar);
+    botonEliminar.forEach(dato=>{
+        // console.log(dato);
+        dato.addEventListener("click", ()=>{
+            eliminarDato(dato.id);
+        });
+    });
 });
+
+
